aff0677e1f46eb325afe4fae3fbb473e9f182052
crybx <crybx@users.noreply.github.com>
2026-03-09 00:21:06 -0400

    fix after calibre update

--- calibre_plugins/epubaholic/container.py
+++ calibre_plugins/epubaholic/container.py
@@ -22,7 +22,11 @@ from calibre.ebooks.conversion.plugins.epub_input import (
     ADOBE_OBFUSCATION, IDPF_OBFUSCATION, decrypt_font)
 from calibre.ebooks.conversion.preprocess import HTMLPreProcessor
 from calibre.ebooks.oeb.base import urlnormalize, OEB_DOCS, XPath, SVG, XLINK
-from calibre.ebooks.oeb.parse_utils import RECOVER_PARSER, NotHTML, parse_html
+from calibre.ebooks.oeb.parse_utils import NotHTML, parse_html
+try:
+    from calibre.ebooks.oeb.parse_utils import RECOVER_PARSER
+except ImportError:
+    RECOVER_PARSER = etree.XMLParser(recover=True)
 from calibre.utils.zipfile import ZipFile, ZIP_DEFLATED, ZIP_STORED
 
 exists, join = os.path.exists, os.path.join
